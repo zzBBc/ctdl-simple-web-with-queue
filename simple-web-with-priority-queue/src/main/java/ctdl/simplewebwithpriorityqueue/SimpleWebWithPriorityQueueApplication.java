d16bc070e3a28b1737a0fb7a540c18f6982cb6cc
zzBBc <zzbbc123@gmail.com>
2020-05-22 13:15:19 +0700

    Initial commit for backend

---
+++ simple-web-with-priority-queue/src/main/java/ctdl/simplewebwithpriorityqueue/SimpleWebWithPriorityQueueApplication.java
@@ -0,0 +1,13 @@
+package ctdl.simplewebwithpriorityqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebWithPriorityQueueApplication {
+
+    public static void main(String[] args) {
+	SpringApplication.run(SimpleWebWithPriorityQueueApplication.class, args);
+    }
+
+}
